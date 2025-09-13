a122b5c3395e161edc00acc1d06fc1c2f13ee2f9
estiakahmed98 <estiakahmed898@gmail.com>
2025-09-13 15:48:32 +0600

    Data Entry Dashboard Done

--- app/data_entry/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function DataEntryPage() {
-  return (
-    <div className="flex justify-center text-2xl font-bold text-gray-600">
-      Data Entry Dashboard
-    </div>
-  );
-}
